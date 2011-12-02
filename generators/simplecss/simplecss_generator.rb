7ddbb520e4cb887384952f403dfb1843d84adaeb
hexudong <hexudong08@gmail.com>
2011-12-02 19:25:22 +0800

    fix simplecss generator bugs

--- generators/simplecss/simplecss_generator.rb
+++ generators/simplecss/simplecss_generator.rb
@@ -3,9 +3,9 @@ class SimplecssGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
       m.directory(File.join('app','views','simplecss'))
+      m.file("navigate.html.erb", File.join("app","views","simplecss","_navigate.html.erb"))
       
       m.file("simplecss.css", File.join("public","stylesheets","simplecss.css"))
-      m.file("navigate.html.erb", File.join("app","views","simplecss","navigate.html.erb"))
       m.file("navigate.jpg",File.join("public","images","navigate.jpg"))
 
       m.readme "readme"
